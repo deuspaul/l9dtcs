7c8f454ab17c5c7a905be01e5c71df96b1563371
Deus Paul <46954959+deuspaul@users.noreply.github.com>
2024-04-03 07:10:00 +0000

    Created app interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { add, divide, multiply, subtract } from '@deuspaul/lesson8';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <input id="num1" />
+        <input id="num2" />
+        <button onClick={ ()=> alert(divide(document.getElementById('num1').value, document.getElementById('num2').value))}>multiply</button>
+        <button onClick={ ()=> alert(divide(document.getElementById('num1').value, document.getElementById('num2').value))}>divide</button>
+        <button onClick={ ()=> alert(subtract(document.getElementById('num1').value, document.getElementById('num2').value))}>subtract</button>
+        <button onClick={ ()=> alert(add(document.getElementById('num1').value, document.getElementById('num2').value))}>add</button>
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
